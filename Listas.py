54334e09f77a5dda78c8e9d593f98edc1b065298
FP-53 <fidelpizart314@gmial.com>
2024-12-14 20:12:06 -0500

    1.2

--- Listas.py
+++ Listas.py
@@ -1,4 +1,4 @@
-### LISTAS ###
+"""     LISTAS      """
 
 mi_lista = list() #mo me gusta usarlo pq causa mas errores que si usara []
 
@@ -44,9 +44,81 @@ print(mi_otra_lista[-3])
 print(mi_otra_lista.count('Fidel'))
 print(mi_lista.count(30))
 
-
+"""Seleccion de varables"""
 #se puede seleccionar los valores de una lista para crear variables 
 #se colocan en el mismo orden que se encuentra dentro de la lista para que encajen correctamente con cada valor 
+#la cantidad de variables deben ser iguales a la cantidad de elementos en la busca o si no debe ser especificado el lugar dentro de la lista
+
+#seleccionar todos los elementos como variable
 edad, altura, nombre, apellido = mi_otra_lista
 print(nombre)
+
+#seleccionar una variable en especifico dentro de la lista, se puede hacer en mayor cantidad usando , para separar
+h = mi_otra_lista[1]
+print(h)
+
+
+"""Concatenar"""
+#Concatenar listas 
+print(mi_lista + mi_otra_lista)
+
+"""Añadir elementos"""
+
+#.append incerta un nuevo elemento al final de la lista 
+mi_otra_lista.append('Perez')
+print(mi_otra_lista)
+
+#.insert debes darle el indice, que quieres insertar
+mi_otra_lista.insert(3, 'Alejandro') 
+print(mi_otra_lista)
+
+"""Eliminar elementos"""
+
+#remueve el elemento que le digas, solo remueve el primer elemento que encuentra con ese nombre
+mi_otra_lista.remove(1.76)
+print(mi_otra_lista)
+
+#.pop elimina el ultimo elemento de la lista 
+mi_otra_lista.pop 
+print(mi_otra_lista)
+#si vuelves a imprimir el .pop te da el valor que quito 
+print(mi_otra_lista.pop())
+#puedes hacer pop a un elemento en concreto dentro de la lista usando () 
+
+#puedes usar pop para crear variables 
+elemento_pop = mi_otra_lista.pop()
+print(elemento_pop)
+
+#pop puede usarse para mover elementos entre listas usando variables 
+print(mi_lista)
+mi_lista.append(elemento_pop)
+print(mi_lista)
+
+mi_otra_lista.append('Pizart')
+mi_otra_lista.append('Perez')
+
+"""Otras formas de eliminar: 
+    del= elimina un elemento segun el indice 
+    clean= borra la lista
+"""
+
+"""Cambiar valores"""
+
+mi_otra_lista[0] = 18
+print(mi_otra_lista)
+
+"""Copiar una lista"""
+
+mi_nueva_lista = mi_otra_lista.copy()
+print(mi_nueva_lista)
+
+
+"""Girar la lista"""
+mi_nueva_lista.reverse()
+print(mi_nueva_lista)
+
+"""Ordenar la lista"""
+#ordena por defecto en orden alfabetico o numerico segun la lista
+mi_nueva_lista.sort()
+print(mi_nueva_lista)
 
---
+++ Tuplas.py
@@ -0,0 +1,15 @@
+"""     TUPLAS      """
+
+#dos formas de nombras una tupla
+my_tuple = tuple()
+my_other_tuple = ()
+
+my_tuple = (35, 17.6, 'Fidel', 'Pizart')
+print(my_tuple)
+print(type(my_tuple))
+
+"""
+A efectos basicos es una lista, la diferencia es:
+    - Las tuplas son listas fijas que no pueden añadir, eliminar o cambiar valores, son valores consantes que no se pueden ca,biqar mediante alguna funcion 
+    - Si se pueden concatenar varias tuplas entre ellas 
+"""
